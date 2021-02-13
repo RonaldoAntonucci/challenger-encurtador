8b9cae96eb29eb5b9959d1c15aa2e7753fb822f5
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2021-02-13 19:27:50 -0300

    test: redirecionando para a url correta

--- tests/main/routes/UrlRoutes.test.ts
+++ tests/main/routes/UrlRoutes.test.ts
@@ -2,10 +2,13 @@ import { ShortUrl, TypeOrmHelper, Url } from '@/infra/db/typeorm';
 import request from 'supertest';
 import faker from 'faker';
 import app from '@/main/config/app';
+import { Connection } from 'typeorm';
 
 describe('Url Routes', () => {
+  let conn: Connection;
   beforeAll(async () => {
     await TypeOrmHelper.connect();
+    conn = TypeOrmHelper.getClient();
   });
 
   afterAll(async () => {
@@ -18,13 +21,31 @@ describe('Url Routes', () => {
   });
 
   describe('POST /encurtador', () => {
-    it('deverá returna 200 no /encurtador', async () => {
+    it('deverá retornar 200 no /encurtador', async () => {
       await request(app)
         .post('/encurtador')
         .send({
           url: faker.internet.url(),
         })
         .expect(200);
+    });
+  });
+
+  describe('GET /', () => {
+    it('deverá returna 302 no /', async () => {
+      const response = await request(app).post('/encurtador').send({
+        url: faker.internet.url(),
+      });
+      const { newUrl } = response.body;
+      const urlSplit = String(newUrl).split('/');
+      const url = urlSplit[urlSplit.length - 1];
+      const res = await request(app).get(`/${url}`).expect(302);
+      expect(res.redirect).toBeTruthy();
+    });
+
+    it('deverá returna 404 no / caso a shortUrl não exista', async () => {
+      const res = await request(app).get(`/notExists`).expect(404);
+      expect(res.redirect).toBeFalsy();
     });
   });
 });
