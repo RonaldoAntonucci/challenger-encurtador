a6d1eda4294d6f7d3e80747a6e69b2802c33ecb9
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2021-02-13 14:25:12 -0300

    test: CryptoAdapter retornando string com tamanho entre 5 e 10 e somente numeros e letras

--- tests/infra/cryptography/CryptoAdapter.spec.ts
+++ tests/infra/cryptography/CryptoAdapter.spec.ts
@@ -7,17 +7,19 @@ describe('CryptoAdapter -- unit', () => {
     sut = new CryptoAdapter();
   });
 
-  it('deverá retornar uma string com o tamanho entre 5 e 10 caracteres', async () => {
+  it('deverá retornar uma string com o tamanho entre 5 e 10 caracteres e contendo somente letras ou números', async () => {
     const promises = [];
     for (let i = 1000; i > 0; i -= 1) {
       promises.push(sut.generateUrl());
     }
 
     const result = await Promise.all(promises);
+    const letttersAndNumber = /^[0-9a-zA-Z]+$/;
 
     result.forEach((response) => {
       expect(response.length).toBeGreaterThanOrEqual(5);
       expect(response.length).toBeLessThanOrEqual(10);
+      expect(letttersAndNumber.test(response)).toBeTruthy();
     });
   });
 });
