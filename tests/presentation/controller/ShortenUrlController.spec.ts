942b17894e1eb59e36dd4ef2f0d63a99f88942ed
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2021-02-13 02:36:16 -0300

    test: ShortenUrlController devera retornar uma newUrl caso tenha successo

--- tests/presentation/controller/ShortenUrlController.spec.ts
+++ tests/presentation/controller/ShortenUrlController.spec.ts
@@ -20,4 +20,22 @@ describe('ShortenUrlController - unit', () => {
     await sut.handle(request);
     expect(encryptUrlSpy.params).toEqual({ url: request.url });
   });
+
+  it('deverá chamar o EncryptUrl com os valores corretos', async () => {
+    const request = makeFakeRequest();
+    await sut.handle(request);
+    expect(encryptUrlSpy.params).toEqual({ url: request.url });
+  });
+
+  it('deverá disparar um erro caso o EncryptUrl dispare um erro', async () => {
+    jest.spyOn(encryptUrlSpy, 'encrypt').mockImplementationOnce(async () => {
+      throw new Error();
+    });
+    await expect(sut.handle(makeFakeRequest())).rejects.toThrow();
+  });
+
+  it('deverá retornar uma nova url caso o EncryptUrl tenha successo', async () => {
+    const response = await sut.handle(makeFakeRequest());
+    expect(response.body.newUrl).toEqual(encryptUrlSpy.result);
+  });
 });
