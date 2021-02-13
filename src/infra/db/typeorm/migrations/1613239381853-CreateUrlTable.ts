acf793be8b4cdfa83544c3ac48b43822f3f4d9d4
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2021-02-13 15:05:16 -0300

    chore: typeorm create url table migration

---
+++ src/infra/db/typeorm/migrations/1613239381853-CreateUrlTable.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUrlTable1613239381853 implements MigrationInterface {
+  private readonly table = new Table({
+    name: 'urls',
+    columns: [
+      {
+        name: 'id',
+        type: 'uuid',
+        isPrimary: true,
+        generationStrategy: 'uuid',
+        default: 'uuid_generate_v4()',
+      },
+      {
+        name: 'url',
+        type: 'varchar',
+        isNullable: false,
+        isUnique: true,
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamp',
+        default: 'now()',
+        isNullable: false,
+      },
+      {
+        name: 'created_at',
+        type: 'timestamp',
+        default: 'now()',
+        isNullable: false,
+      },
+    ],
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.table);
+  }
+}
