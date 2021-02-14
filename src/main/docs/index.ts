0a8a4dd554a0ea2c8ca8740727bbb69c5c34231d
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2021-02-14 14:02:51 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Encurtador',
+    description: 'API desenvolvida para um desafio de um encurtador de urls',
+    version: '1.0.0',
+  },
+};
